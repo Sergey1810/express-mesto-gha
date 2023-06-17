7f6a8f3e8fbb6d93aa5926d3caa7d5257b567c35
Sergey Lisovcev <deadlook15@yandex.ru>
2023-06-17 23:21:22 +0300

    feat: server_rev1

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const cardRoutes = require('./cards');
 
 router.use('/users', userRoutes);
 router.use('/cards', cardRoutes);
-route.use('/', function(req, res) {
+route.use('*', function(req, res) {
         return res.status(404).send({ message: "Не корректный путь" });
 } )
 
